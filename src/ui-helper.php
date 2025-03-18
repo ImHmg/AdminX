68dd9e5dd907d8b7d8402751c90fa7bc4edd122e
Hasitha <dev.hasitha@gmail.com>
2025-03-18 08:49:01 +0800

    Add share connection

--- src/ui-helper.php
+++ src/ui-helper.php
@@ -1,37 +1,39 @@
 <?php
 
-function getDatabasesOptions() {
-        $db = DBClient::get();
-        $schemas = $db->getDatabases();
-        $out = "";
-        $out .= '<option value="">Select Schema</option> \n';
+function getDatabasesOptions()
+{
+    $db = DBClient::get();
+    $schemas = $db->getDatabases();
+    $out = "";
+    $out .= '<option value="">Select Schema</option> \n';
 
-        foreach($schemas as $c) {
-           if($db->schema == $c) {
-            $out .= '<option value="'.$c.'" selected>'. $c .'</option> \n';
-           }else{
-            $out .= '<option value="'.$c.'">'. $c .'</option> \n';
-           }
+    foreach ($schemas as $c) {
+        if ($db->schema == $c) {
+            $out .= '<option value="' . $c . '" selected>' . $c . '</option> \n';
+        } else {
+            $out .= '<option value="' . $c . '">' . $c . '</option> \n';
         }
-        return $out;
+    }
+    return $out;
 }
 
-function getTablesRow() {
+function getTablesRow()
+{
     $db = DBClient::get();
     $vals = $db->getTables();
     $out = "";
-    foreach($vals as $c) {
-       $out .= '         <tr class="dashboard-table-row">
+    foreach ($vals as $c) {
+        $out .= '         <tr class="dashboard-table-row">
                             <td>
                                 <div class="dropdown dashboard-table-row-container">
-                                    <a type="button" class="dropdown-toggle dashboard-table-row-item" data-bs-toggle="dropdown" data-table-name="'.$c.'" style="text-decoration: none; color: black;">
+                                    <a type="button" class="dropdown-toggle dashboard-table-row-item" data-bs-toggle="dropdown" data-table-name="' . $c . '" style="text-decoration: none; color: black;">
                                     <img style="width: 15px" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAkElEQVR4nO3WQQqAMAxE0dz/WBbPFXcuxCGVFJra/yA7kYRMIWYAvjrNzItX6xnEF6lQ94edpv3PGeQdGxGIVhbREohWFtES9o2WF6/Q7Aado/Hvb2QUBhHYSBbREohWFtESiNby0fLiFZrdoHP9PnA0Vn3sozCIwEayiJZAtLKIlrDfrdUKNOlBHT2DALDbBX2sGRStwMkLAAAAAElFTkSuQmCC" alt="table-1">
-                                        '. $c .'
+                                        ' . $c . '
                                     </a>
                                     <ul class="dropdown-menu dropdown-menu-dark">
-                                     <h6 class="dropdown-header">'. $c .' table</h6>
-                                      <li><a class="dropdown-item" href="index.php?action_view_100_rows=1&table='. $c .'">View 100 Rows</a></li>
-                                      <li><a class="dropdown-item" href="index.php?action_desc_table=1&table='. $c .'">Describe</a></li>
+                                     <h6 class="dropdown-header">' . $c . ' table</h6>
+                                      <li><a class="dropdown-item" href="index.php?action_view_100_rows=1&table=' . $c . '">View 100 Rows</a></li>
+                                      <li><a class="dropdown-item" href="index.php?action_desc_table=1&table=' . $c . '">Describe</a></li>
                                     </ul>
                                 </div>
                             </td>
@@ -42,9 +44,10 @@ function getTablesRow() {
 
 
 
-function getThemeCss() {
+function getThemeCss()
+{
     $css = '';
-    if(file_exists('theme.css')) {
+    if (file_exists('theme.css')) {
         $css = file_get_contents('theme.css');
     }
 
@@ -52,11 +55,37 @@ function getThemeCss() {
     return $theme . $css;
 }
 
-function getDashboardHtml() {
+function getDashboardHtml()
+{
+
+    function getConnectionUrl()
+    {
+        $dbDetails = getDatabaseDetails();
+
+        $queryParams = [
+            'p' => 'login',
+            'server' => $dbDetails['server'] ?? '',
+            'port' => $dbDetails['port'] ?? '',
+            'user' => $dbDetails['user'] ?? '',
+        ];
+        if (!empty($dbDetails['schema'])) {
+            $queryParams['schema'] = $dbDetails['schema'];
+        }
+        $url = http_build_query($queryParams);
+        return $url;
+    }
+
+    $dbDeatils = getDatabaseDetails();
+
+
     $template = "#FSTRINCLUDE assets/dashboard.html";
     $template = str_replace('__DB_OPTIONS__', getDatabasesOptions(), $template);
     $template = str_replace('__TABLES__', getTablesRow(), $template);
     $template = str_replace('__THEME_CSS__', '<style>' . getThemeCss() . '</style>', $template);
+    $template = str_replace('__CONNECTION_SERVER__', $dbDeatils['server'], $template);
+    $template = str_replace('__CONNECTION_PORT__', $dbDeatils['port'], $template);
+    $template = str_replace('__CONNECTION_USER__', $dbDeatils['user'], $template);
+    $template = str_replace('__CONNECTION_URL__', getConnectionUrl(), $template);
     return $template;
 }
 
